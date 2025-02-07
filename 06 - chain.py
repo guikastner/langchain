54804d2edffd976a3d4168ba581108c5ce7d9ab2
guikastner <guilherme@kastner.com.br>
2025-02-07 18:35:59 -0300

    ajuste fusca

---
+++ 06 - chain.py
@@ -0,0 +1,20 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import authorization
+
+api_key = authorization.apikey
+os.environ['OPENAI_API_KEY'] = api_key
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+prompt_template = PromptTemplate.from_template(
+    'me fale sobre o carro {carro}',
+)
+
+runnable_sequence = prompt_template | model | StrOutputParser()
+
+result = runnable_sequence.invoke({"carro": "fusca 1958"})
+
+print(result)
